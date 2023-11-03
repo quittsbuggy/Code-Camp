2e597616f1afbba8f1d7ac8e4e049e8c07d13678
Garett Lynch <glynch.git@gmail.com>
2023-11-03 12:36:33 -0400

    update files for end of lecture 1

---
+++ CS50_py/calculator.py
@@ -0,0 +1,8 @@
+x = float(input("What is x? "))
+y = float(input("What is y? "))
+
+#round the result of x +y
+z = round(x + y)
+
+#format with american style deliniation
+print(f"{z:,}")
--- CS50_py/hello.py
+++ CS50_py/hello.py
@@ -1,5 +1,8 @@
 #Ask User for name
 name = input("What's your name? ").strip().title()
 
+#Split user's name into first and last name
+first, last = name.split(" ")
+
 #Say hello to user
-print("hello", name, end='\n\n')
+print("hello", first, last)
